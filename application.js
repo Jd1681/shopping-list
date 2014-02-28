82057cc74685598e7d9090872effb0fb0061ee1a
Jd1681 <jeremy.duprey@gmail.com>
2014-02-28 13:36:01 -0500

    errors

--- application.js
+++ application.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
   $('.purchase').mousedown(function() {
  	$(this).parent().toggleClass('bought need');
  });
+  $('.add').mousedown(function() {
+  	var newItem = document.getElementbyID("addItems").string;
+  	$('#list').append(newItem);
+  });
 });
