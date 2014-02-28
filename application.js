7d81c1c9bf84195b11539a40625e04e407c8218f
Jd1681 <jeremy.duprey@gmail.com>
2014-02-28 11:34:00 -0500

    step2

--- application.js
+++ application.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
  $('.delete').mousedown(function() {
  	$(this).parent().hide();
  });
+  $('.purchase').mousedown(function() {
+ 	$(this).parent().toggleClass('bought need');
+ });
 });
