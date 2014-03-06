dd40fc6e5061ae258683474508d451e846ace10c
Jd1681 <jeremy.duprey@gmail.com>
2014-03-06 08:44:10 -0500

    finishednearly

--- application.js
+++ application.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   $('.purchase').mousedown(function() {
  	$(this).parent().toggleClass('bought need');
  });
-  $('.add').mousedown(function() {
-  	var newItem = document.getElementbyID("addItems").val();
-  	$('#list').append(newItem);
-  });
+  $('.add').mousedown(function(){
+	var addthisItem = $("#addItems").val();
+	$("#list").append("<div class='items need'>" + addthisItem + "<div class='purchase'></div><div class='delete'>-</div></div>");
+	});
 });
